b9b4b5309f672c84dab125cad95a6fa9651a65ab
Nikita <nikitabylnov@inbox.ru>
2020-02-01 15:36:13 +0300

    last work

--- JavaScriptPart.js
+++ JavaScriptPart.js
@@ -1,10 +1,25 @@
+function ClickInformNikita() { jQuery('#member_inform').html("Никита"); }
+function ClickInformArtur() { jQuery('#member_inform').html("Артур"); }
+function ClickInformRuslan() { jQuery('#member_inform').html("Руслан"); }
+function ClickInformAnsar() { jQuery('#member_inform').html("Ансар"); }
+function ClickInformMotya() { jQuery('#member_inform').html("Матвей"); }
+function ClickInformLesha() { jQuery('#member_inform').html("Лёша"); }
+function ClickInformKarim() { jQuery('#member_inform').html("Карим"); }
+function ClickInformBatynur() { jQuery('#member_inform').html("Батыйнур"); }
+function ClickInformSanya() { jQuery('#member_inform').html("Саня"); }
+function ClickInformDanya() { jQuery('#member_inform').html("Данил"); }
+function ClickInformGenady() { jQuery('#member_inform').html("Генрих"); }
+
+
+
+
 jQuery('document').ready(function() {
 	
 	var name = prompt("Введите ваше имя пользователя"), password = "КОММУНИЗМ", PasswordTest = prompt("Пожалуйста, введите ваш пароль");
 	
 	if (password == PasswordTest)
 	{	
-		if (name.length <= 15) { jQuery('#UserName').html(name); jQuery('#Hello').html("Приветствуем тебя, товарищ" + " " + name + "!!!"); }
+		if (name.length <= 12) { jQuery('#UserName').html(name); jQuery('#Hello').html("Приветствуем тебя, товарищ" + " " + name + "!!!"); }
 		else { jQuery('#UserName').html("Слишком длинное имя пользователя"); }
 		
 		alert("Вход в аккаунт успешно выполнен. Добро пожаловать!");
@@ -13,8 +28,11 @@ jQuery('document').ready(function() {
 	}
 	else
 	{
-		alert("Неверный пароль");
-		jQuery('body').remove();
+		jQuery('#base').remove();
+		$('body').append('<img src="Albert_Lovyshka.jpg" id="Albert_Lovyshka"/>');
 	}
+	
+	
+	
 	
 });
